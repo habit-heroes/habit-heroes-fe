640d5c3bb1b70d4f347f8328beb28c45db8f7a63
Brandon Doza <brandondoza@brandos-m2-mbp.lan>
2024-07-03 15:44:50 -0600

    import main font

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,7 +1,17 @@
 export default function Login() {
     return (
         <div className='login-page'>
-            <h1>This is the login page</h1>
+            <h1 className='page-title'>Habit Hero</h1>
+            <form>
+                <input
+                type='text'
+                placeholder='Email'
+                />
+                <input
+                type='text'
+                placeholder='Password'
+                />               
+            </form>
         </div> 
     )
 }
