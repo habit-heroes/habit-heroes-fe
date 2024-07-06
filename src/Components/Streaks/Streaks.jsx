d0bbc816407465afd038676067f2caf15d462cb8
Brandon Doza <brandondoza@brandos-m2-mbp.lan>
2024-07-06 10:19:45 -0600

    add Streaks component wire frame

---
+++ src/Components/Streaks/Streaks.jsx
@@ -0,0 +1,13 @@
+export default function Streaks() {
+    return (
+        <div className='streaks'>
+            <ul className='streak-list'>
+                <li>Streak 1</li>
+                <li>Streak 2</li>
+                <li>Streak 3</li>
+                <li>Streak 4</li>
+                <li>Streak 5</li>
+            </ul>
+        </div>
+    )
+}
