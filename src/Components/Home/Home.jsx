0cb5b17d7e763ea33490b815368abbf826a5f2f2
Brandon Doza <brandondoza@brandos-m2-mbp.lan>
2024-07-06 10:22:21 -0600

    add TodaysHabits component wire frame

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Streaks from "../Streaks/Streaks"
+import TodaysHabits from "../TodaysHabits/TodaysHabits"
 
 export default function Home() {
     return (
         <div className='home'>
             <Streaks />
+            <TodaysHabits />
         </div>
     )
 }
---
+++ src/Components/TodaysHabits/TodaysHabits.jsx
@@ -0,0 +1,13 @@
+export default function TodaysHabits() {
+    return (
+        <div className='streaks'>
+            <ul className='streak-list'>
+                <li>Habit 1</li>
+                <li>Habit 2</li>
+                <li>Habit 3</li>
+                <li>Habit 4</li>
+                <li>Habit 5</li>
+            </ul>
+        </div>
+    )
+}
