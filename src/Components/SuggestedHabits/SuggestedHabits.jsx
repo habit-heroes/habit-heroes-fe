3578c4cf12df83d0f910e74c4c5c3fa8678b9b33
Brandon Doza <brandondoza@brandos-m2-mbp.lan>
2024-07-07 16:45:05 -0600

    add redux logic/global state to suggested habits

--- src/Components/SuggestedHabits/SuggestedHabits.jsx
+++ src/Components/SuggestedHabits/SuggestedHabits.jsx
@@ -1,9 +1,38 @@
 import './SuggestedHabits.css'
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchHabits } from '../../app/habitsSlice';
 
 export default function SuggestedHabits() {
+  const habits = useSelector((state) => state.habits.habits); //this is where all the habits are acessed. This is the global state
+  const loading = useSelector((state) => state.habits.loading);
+  const error = useSelector((state) => state.habits.error);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchHabits());
+  }, [dispatch]);
+
+  if (loading) {
+    return <p>Loading habits...</p>;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
+  //const allHabits = habits.map(habit => {
+// return (
+//    <HabitCard
+//      key={habit.id}
+//      id={habit.id}
+//      title={habit.title}
+//      etc......
+//      )})
     return (
         <div className='suggested-habits'>
             <ul className='habit-list'>
+                {/* {allHabits} */}
                 <li>Habit 1</li>
                 <li>Habit 2</li>
                 <li>Habit 3</li>
--- src/app/store.js
+++ src/app/store.js
@@ -1,4 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
+import habitsReducer from './habitsSlice'
 
 export const store = configureStore({
     reducer: {
