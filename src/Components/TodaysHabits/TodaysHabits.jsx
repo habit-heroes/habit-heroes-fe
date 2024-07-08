5b446b927968ec709a1f5716934d7d5f2a2e225a
Brandon Doza <brandondoza@brandos-m2-mbp.lan>
2024-07-08 10:36:03 -0600

    add redux logic to todays habits component

--- src/Components/TodaysHabits/TodaysHabits.jsx
+++ src/Components/TodaysHabits/TodaysHabits.jsx
@@ -1,6 +1,25 @@
 import './TodaysHabits.css'
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchUserHabits } from '../../app/userHabitsSlice';
 
 export default function TodaysHabits() {
+    const userHabits = useSelector((state) => state.userHabits.userHabits); 
+    const loading = useSelector((state) => state.userHabits.loading);
+    const error = useSelector((state) => state.userHabits.error);
+    const dispatch = useDispatch();
+  
+    useEffect(() => {
+      dispatch(fetchUserHabits());
+    }, [dispatch]);
+  
+    if (loading) {
+      return <p>Loading habits...</p>;
+    }
+  
+    if (error) {
+      return <p>Error: {error}</p>;
+    }
     return (
         <div className='todays-habits'>
             <ul className='suggested-habit-list'>
