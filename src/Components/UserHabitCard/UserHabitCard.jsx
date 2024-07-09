4fbcfa9702ee01ae2905a8faa187ac858ec9e204
Brandon Doza <brandondoza@brandos-m2-mbp.lan>
2024-07-09 10:33:09 -0600

    add framework for user habit card

---
+++ src/Components/UserHabitCard/UserHabitCard.jsx
@@ -0,0 +1,13 @@
+import './UserHabitCard.css'
+
+
+
+export default function UserHabitCard() {
+  return (
+    <div>
+        <h1>Habit Title</h1>
+        <h2>Habit Category</h2>
+        <button>mark habit as completed</button>
+    </div>
+  )
+}
