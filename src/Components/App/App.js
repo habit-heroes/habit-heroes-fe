70c833a66f9452b3716dec1f316d2d55ea5f2328
laurel Bonal <laurelbonal@gmail.com>
2024-07-09 09:54:27 -0600

    Implement router

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,15 +1,27 @@
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import { useState } from 'react';
 import './App.css';
 import Login from '../Login/Login';
 import Home from '../Home/Home';
-import Header from '../Header/Header'
+import Header from '../Header/Header';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+  };
+
   return (
-    <div className="App">
-      <Header />
-      <Login />
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/login" element={isLoggedIn ? <Navigate to="/" /> : <Login onLogin={handleLogin} />} />
+          <Route path="/" element={isLoggedIn ? <Home /> : <Navigate to="/login" />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
