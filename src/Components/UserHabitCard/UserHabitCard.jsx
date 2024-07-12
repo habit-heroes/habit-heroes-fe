e08c87926bee562a94ccc760e6bbee2079d00090
Brandon Doza <141676838+BrandonDoza@users.noreply.github.com>
2024-07-12 15:43:21 -0600

    add functionality to allow a user to mark a habit as done

--- src/Components/UserHabitCard/UserHabitCard.jsx
+++ src/Components/UserHabitCard/UserHabitCard.jsx
@@ -1,15 +1,27 @@
 import './UserHabitCard.css'
+import { useState } from 'react'
 
 
 
 export default function UserHabitCard({userHabit}) {
+  const [counter, setCounter] = useState(0)
   console.log(userHabit, "USER HABIT SINGULAR")
-  
+
+  function incrementCounter() {
+      setCounter((prevCounter) => {
+        return prevCounter + 1
+      })
+  }
+  console.log('COUNTER', counter)
   return (
     <div className='user-habit-card'>
         <h1 className='habit-title'>{userHabit.name}</h1>
         {/* <h2 className='habit-category'>{userHabit.category}</h2> */}
-        <button className='habit-buttons'>Done</button>
+        <button className='habit-buttons' onClick={incrementCounter}>Done</button>
+        <div className='counter'>
+          <p className='times-done'>Times Done</p>
+        <p className='count'>{counter}</p>
+        </div>
     </div>
   )
 }
