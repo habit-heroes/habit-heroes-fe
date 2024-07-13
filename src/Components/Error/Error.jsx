ee785f280d584478953b66a851fc06846db1db59
laurel Bonal <laurelbonal@gmail.com>
2024-07-13 10:13:27 -0600

    Add error page for invalid url

---
+++ src/Components/Error/Error.jsx
@@ -0,0 +1,10 @@
+import './Error.css'
+
+export default function Error(){
+    return(
+        <div className='error'>
+            <h2>404</h2>
+            <p>The page you are looking for cannot be found</p>
+        </div>
+    )
+}
